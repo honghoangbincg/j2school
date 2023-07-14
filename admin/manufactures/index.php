b799f17af402b1b0074dd5c82274172535a0132f
Hoang Hong <honghoang@bincg.com>
2023-07-14 16:28:52 +0700

    first commit

---
+++ admin/manufactures/index.php
@@ -0,0 +1,7 @@
+<?php include '../header.php'?>
+<?php include_once '../menu.php' ?>
+   <div class="container">
+   <h1>Đây là giao diện quản lý nhà sản xuất</h1>
+    <a href="form_insert.php">Add</a>
+   </div>
+<?php include '../footer.php'?>
---
+++ admin/root/index.php
@@ -0,0 +1,6 @@
+<?php include '../header.php'?>
+<?php include_once '../menu.php' ?>
+   <div class="container">
+   <h1>Đây là giao diện admin</h1>
+   </div>
+<?php include '../footer.php'?>
